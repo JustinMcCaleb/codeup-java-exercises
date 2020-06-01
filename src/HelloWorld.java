e7861c323ed0a130dbe58fb23e7919b78e6c0028
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-01 15:21:19 -0500

    completed Java Syntax, Types, Values Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,55 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+
+        int myFavoriteNumber = 666;
+        System.out.println(myFavoriteNumber);
+
+//        String myString = 3.14159;
+//        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        x /= y;
+//        y -= x;
+
+        byte wrongNum = 127;
+        System.out.println(++wrongNum);
+        System.out.println(++wrongNum);
+        System.out.println(++wrongNum);
+        System.out.println(++wrongNum);
+        System.out.println(++wrongNum);
+
+
     }
 }
