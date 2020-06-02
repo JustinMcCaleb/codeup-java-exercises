67c2fbd04bac0b4005cb5f4d46856e8d6ac18327
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-02 14:16:38 -0500

    uncommented exercise code

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,53 +5,51 @@ public class ConsoleExercises {
     public static void main(String[] args) {
 
 
-//        double pi = 3.14159;
-//
-//        System.out.printf("The value of pi is approximately %.2f.\n", pi);
-//
+        double pi = 3.14159;
+
+        System.out.printf("The value of pi is approximately %.2f.\n", pi);
+
 //
 //    // EXPLORE THE SCANNER CLASS
 //        //Part 1
 //
         Scanner sc = new Scanner(System.in);
 //
-//        System.out.println("Please enter an integer: ");
-//
-//        int userInput = sc.nextInt();
-//
-//        System.out.println(userInput);
+        System.out.println("Please enter an integer: ");
+
+        int userInput = sc.nextInt();
+
+        System.out.println(userInput);
 //
 //
 //
 //        //Part 2
-//        System.out.println("Enter three words: ");
-//
-//        String firstInputWord = sc.next();
-//        String secondInputWord = sc.next();
-//        String thirdInputWord = sc.nextLine();
-//
-//        System.out.println(firstInputWord);
-//        System.out.println(secondInputWord);
-//        System.out.println(thirdInputWord);
+        System.out.println("Enter three words: ");
+
+        String firstInputWord = sc.next();
+        String secondInputWord = sc.next();
+        String thirdInputWord = sc.nextLine();
+
+        System.out.println(firstInputWord);
+        System.out.println(secondInputWord);
+        System.out.println(thirdInputWord);
 //
 //
 //
 //        //Part 3
-//        System.out.println("Please enter a sentence: ");
-//
-//        String userInputSentence = sc.nextLine();
-//        System.out.println(userInputSentence);
+        System.out.println("Please enter a sentence: ");
+
+        String userInputSentence = sc.nextLine();
+        System.out.println(userInputSentence);
 
 
     //Calculate the perimeter and area of Codeup's classrooms
         //Part 1
         System.out.println("Enter the length: ");
         int lengthInput = Integer.parseInt(sc.nextLine());
-//        int lengthInputInt = Integer.parseInt(lengthInput);
 
         System.out.println("Enter the width: ");
         int widthInput = Integer.parseInt(sc.nextLine());
-//        int widthInputInt = Integer.parseInt(widthInput);
 
         System.out.printf("The area of your rectangle is: %s\n", lengthInput * widthInput);
         System.out.printf("The perimeter of your rectangle is: : %s\n", (widthInput * 2) + (lengthInput *2));
