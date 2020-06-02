57b23534dc0ac57cf431a8b99aa54c49fa1d3e70
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-02 16:02:45 -0500

    refactored and removed comments

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -9,38 +9,44 @@ public class ConsoleExercises {
 
         System.out.printf("The value of pi is approximately %.2f.\n", pi);
 
-//
-//    // EXPLORE THE SCANNER CLASS
-//        //Part 1
-//
+
+    // EXPLORE THE SCANNER CLASS
+        //Part 1
+
         Scanner sc = new Scanner(System.in);
-//
-        System.out.println("Please enter an integer: ");
+
+        System.out.print("Please enter an integer: ");
 
         int userInput = sc.nextInt();
 
         System.out.println(userInput);
-//
-//
-//
-//        //Part 2
+
+
+
+        //Part 2
         System.out.println("Enter three words: ");
 
         String firstInputWord = sc.next();
         String secondInputWord = sc.next();
-        String thirdInputWord = sc.nextLine();
+        String thirdInputWord = sc.next();
 
-        System.out.println(firstInputWord);
-        System.out.println(secondInputWord);
-        System.out.println(thirdInputWord);
-//
-//
-//
-//        //Part 3
+//        System.out.println(firstInputWord);
+//        System.out.println(secondInputWord);
+//        System.out.println(thirdInputWord);
+
+        System.out.printf("%s%n%s%n%s%n", firstInputWord, secondInputWord, thirdInputWord);
+
+
+
+
+        //Part 3
+        sc.nextLine(); //clears out next line
         System.out.println("Please enter a sentence: ");
 
         String userInputSentence = sc.nextLine();
         System.out.println(userInputSentence);
+
+
 
 
     //Calculate the perimeter and area of Codeup's classrooms
