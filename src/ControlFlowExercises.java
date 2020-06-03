163bf21227bd32ef8b909d06f53e9c2ffe58fe6c
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-03 18:27:44 -0500

    completed Control Statements and Loops Exercise 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class ControlFlowExercises {
@@ -51,47 +53,90 @@ public class ControlFlowExercises {
         ///////////////////////
 
 
-            //for (int n = 1; n <= 100; n++) {
-            //    if (n % 3 == 0 && n % 5 == 0) {
-            //        System.out.println("FizzBuzz");
-            //    }else if (n % 3 == 0) {
-            //        System.out.println("Fizz");
-            //    }else if (n % 5 == 0) {
-            //        System.out.println("Buzz");
-            //    }else {
-            //        System.out.println(n);
-            //    }
-            //}
-
-            Scanner sc = new Scanner(System.in);
-
-            System.out.print("What number would you like to go up to? ");
-
-            int userInputInt = sc.nextInt();
-
-            System.out.println("Here is your table");
-
-            String header1 = "number";
-            String header2 = "squared";
-            String header3 = "cubed";
-
-            String seperator = "|";
-            String dashes1 = "------";
-            String dashes2 = "-------";
-            String dashes3 = "-----";
-
-            System.out.printf("%-6s %s %-7s %s %-5s\n", header1, seperator, header2, seperator, header3);
-            System.out.printf("%-6s %s %-7s %s %-5s\n", dashes1, seperator, dashes2, seperator, dashes3);
+                //for (int n = 1; n <= 100; n++) {
+                //    if (n % 3 == 0 && n % 5 == 0) {
+                //        System.out.println("FizzBuzz");
+                //    }else if (n % 3 == 0) {
+                //        System.out.println("Fizz");
+                //    }else if (n % 5 == 0) {
+                //        System.out.println("Buzz");
+                //    }else {
+                //        System.out.println(n);
+                //    }
+                //}
 
 
-            for(int a = 1; a <= userInputInt; a++){
-                    int number = a;
+            //Display a table of powers
 
-                    System.out.printf("%-6d %s %-7d %s %-5d\n", number, seperator, number * number, seperator, number * number * number);
+                    Scanner sc = new Scanner(System.in);
+
+                    System.out.print("What number would you like to go up to? ");
+
+                    int userInputInt = sc.nextInt();
+
+                    System.out.println("Here is your table");
+
+                    String header1 = "number";
+                    String header2 = "squared";
+                    String header3 = "cubed";
+
+                    String seperator = "|";
+                    String dashes1 = "------";
+                    String dashes2 = "-------";
+                    String dashes3 = "-----";
+
+                    System.out.printf("%-6s %s %-7s %s %-5s\n", header1, seperator, header2, seperator, header3);
+                    System.out.printf("%-6s %s %-7s %s %-5s\n", dashes1, seperator, dashes2, seperator, dashes3);
+
+
+                    for(int a = 1; a <= userInputInt; a++){
+                            int number = a;
+
+                            System.out.printf("%-6d %s %-7d %s %-5d\n", number, seperator, number * number, seperator, number * number * number);
+                    }
+
+
+
+
+                System.out.print("Enter a numerical grade between 0 - 100: ");
+
+                    byte grade = sc.nextByte();
+
+                    gradeLetterLogic(grade);
+
+                System.out.print("Do you wish to continue? [ Y / N ]");
+                    String userContinue = sc.next();
+                    if(userContinue.equalsIgnoreCase("Y")) {
+                       gradeLoop();
+                    }
+
+
+    }
+
+    public static void gradeLetterLogic(byte grade) {
+            if (grade <= 100 && grade >= 88) {
+                System.out.println("A");
+            }else if (grade <= 87 && grade >= 80){
+                System.out.println("B");
+            }else if (grade <= 79 && grade >= 67) {
+                System.out.println("C");
+            }else if (grade <= 66 && grade >= 60) {
+                System.out.println("D");
+            }else {
+                System.out.println("F");
             }
+    }
 
-
-
-
+    public static void gradeLoop () {
+        Scanner sc = new Scanner(System.in);
+        boolean startStop = true;
+        while (startStop) {
+            System.out.print("Enter a numerical grade between 0 - 100: ");
+            byte grade2 = sc.nextByte();
+            gradeLetterLogic(grade2);
+            System.out.print("Do you wish to continue? [ Y / N ]");
+            String userContinue = sc.next();
+            startStop = userContinue.equalsIgnoreCase("Y");
+        }
     }
 }
