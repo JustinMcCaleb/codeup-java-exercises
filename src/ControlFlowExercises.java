a6badfe781c7b6f63df0dbf68585b5fe40ae15ee
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-03 17:33:04 -0500

    completed fizzbuzz and table of powers exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
@@ -49,17 +51,47 @@ public class ControlFlowExercises {
         ///////////////////////
 
 
-            for (int n = 0; n <= 100; n++) {
-                if (n % 3 == 0 && n % 5 == 0) {
-                    System.out.println("FizzBuzz");
-                }else if (n % 3 == 0) {
-                    System.out.println("Fizz");
-                }else if (n % 5 == 0) {
-                    System.out.println("Buzz");
-                }else {
-                    System.out.println(n);
-                }
+            //for (int n = 1; n <= 100; n++) {
+            //    if (n % 3 == 0 && n % 5 == 0) {
+            //        System.out.println("FizzBuzz");
+            //    }else if (n % 3 == 0) {
+            //        System.out.println("Fizz");
+            //    }else if (n % 5 == 0) {
+            //        System.out.println("Buzz");
+            //    }else {
+            //        System.out.println(n);
+            //    }
+            //}
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("What number would you like to go up to? ");
+
+            int userInputInt = sc.nextInt();
+
+            System.out.println("Here is your table");
+
+            String header1 = "number";
+            String header2 = "squared";
+            String header3 = "cubed";
+
+            String seperator = "|";
+            String dashes1 = "------";
+            String dashes2 = "-------";
+            String dashes3 = "-----";
+
+            System.out.printf("%-6s %s %-7s %s %-5s\n", header1, seperator, header2, seperator, header3);
+            System.out.printf("%-6s %s %-7s %s %-5s\n", dashes1, seperator, dashes2, seperator, dashes3);
+
+
+            for(int a = 1; a <= userInputInt; a++){
+                    int number = a;
+
+                    System.out.printf("%-6d %s %-7d %s %-5d\n", number, seperator, number * number, seperator, number * number * number);
             }
+
+
+
 
     }
 }
