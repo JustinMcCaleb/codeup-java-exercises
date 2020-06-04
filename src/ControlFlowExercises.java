d97e0af390b70fd80176a5348d198d425404e41d
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-04 11:26:24 -0500

    refactored fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -51,17 +51,17 @@ public class ControlFlowExercises {
         ///////////////////////
 
 
-                //for (int n = 1; n <= 100; n++) {
-                //    if (n % 15 == 0) {
-                //        System.out.println("FizzBuzz");
-                //    }else if (n % 5 == 0) {
-                //        System.out.println("Fizz");
-                //    }else if (n % 3 == 0) {
-                //        System.out.println("Buzz");
-                //    }else {
-                //        System.out.println(n);
-                //    }
-                //}
+//                for (int n = 1; n <= 100; n++) {
+//                    if (n % 15 == 0) {
+//                        System.out.println("FizzBuzz");
+//                    }else if (n % 5 == 0) {
+//                        System.out.println("Buzz");
+//                    }else if (n % 3 == 0) {
+//                        System.out.println("Fizz");
+//                    }else {
+//                        System.out.println(n);
+//                    }
+//                }
 
 
             //Display a table of powers
