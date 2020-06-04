c710ce2e8a83a6a87ac85a922a7e162d2e915520
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-04 11:24:21 -0500

    refactored fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -52,11 +52,11 @@ public class ControlFlowExercises {
 
 
                 //for (int n = 1; n <= 100; n++) {
-                //    if (n % 3 == 0 && n % 5 == 0) {
+                //    if (n % 15 == 0) {
                 //        System.out.println("FizzBuzz");
-                //    }else if (n % 3 == 0) {
-                //        System.out.println("Fizz");
                 //    }else if (n % 5 == 0) {
+                //        System.out.println("Fizz");
+                //    }else if (n % 3 == 0) {
                 //        System.out.println("Buzz");
                 //    }else {
                 //        System.out.println(n);
