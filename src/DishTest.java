3777d96f5bf0f4fff7aa6475473ca3ee406a8e9a
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-09 17:04:30 -0500

    cleaned up code in DishTest

--- src/DishTest.java
+++ src/DishTest.java
@@ -1,44 +1,44 @@
 public class DishTest {
     public static void main(String[] args) {
-        DishTools dt = new DishTools();
 
         //Using Getters and Setters to set property values
-        Dish dish1 = new Dish();
+            Dish dish1 = new Dish();
 
-        dish1.setCostInCents(50);
-        dish1.setNameOfDish("Kitchen Sink");
-        dish1.setWouldRecommend(false);
+            dish1.setCostInCents(50);
+            dish1.setNameOfDish("Kitchen Sink");
+            dish1.setWouldRecommend(false);
 
-        dish1.printSummary();
+            dish1.printSummary();
 
         //Using Getters and Setters to set property values
-        Dish dish2 = new Dish();
+            Dish dish2 = new Dish();
 
-        dish2.setCostInCents(95);
-        dish2.setNameOfDish("Breakfast Taco");
-        dish2.setWouldRecommend(true);
+            dish2.setCostInCents(95);
+            dish2.setNameOfDish("Breakfast Taco");
+            dish2.setWouldRecommend(true);
 
-        dish2.printSummary();
-
-        //Using Constructor to set property values
-        Dish dish3 = new Dish(25, "Chips and Salsa", true);
-
-        dish3.printSummary();
+            dish2.printSummary();
 
         //Using Constructor to set property values
-        Dish dish4 = new Dish(66, "Chicken Strips", true);
+            Dish dish3 = new Dish(25, "Chips and Salsa", true);
 
-        dish4.printSummary();
+            dish3.printSummary();
 
-        DishTools.shoutDishName(dish1);
+        //Using Constructor to set property values
+            Dish dish4 = new Dish(66, "Chicken Strips", true);
 
-        DishTools.analyzeDishCost(dish2);
-        DishTools.analyzeDishCost(dish3);
+            dish4.printSummary();
 
-        DishTools.flipRecommendation(dish1);
-        dish1.printSummary();
+        //Using the static fields defined in the DishTools class
+            DishTools.shoutDishName(dish1);
 
-        DishTools.flipRecommendation(dish2);
-        dish2.printSummary();
+            DishTools.analyzeDishCost(dish2);
+            DishTools.analyzeDishCost(dish3);
+
+            DishTools.flipRecommendation(dish1);
+            dish1.printSummary();
+
+            DishTools.flipRecommendation(dish2);
+            dish2.printSummary();
     }
 }
