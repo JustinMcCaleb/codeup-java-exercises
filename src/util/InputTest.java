b469822001a3d9422e709c9f22bf3841c2d4b425
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-10 21:04:18 -0500

    OOP: added double methods

---
+++ src/util/InputTest.java
@@ -0,0 +1,33 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+
+
+        Input in = new Input();
+        System.out.println("Please enter a word");
+        String userInputString = in.getString();
+        System.out.println(userInputString);
+
+        System.out.println("Will you continue?");
+        boolean trueFalse = in.yesNo();
+        System.out.println(trueFalse);
+
+        System.out.println("Please enter a whole number");
+        int userInputInt = in.getInt();
+        System.out.println("You entered " + userInputInt);
+
+        System.out.println("Enter an integer between 1 and 10");
+        int numRange = in.getInt(1, 10);
+        System.out.println(numRange + " is between 1 and 1");
+
+        System.out.println("Please enter a number w/ decimals.");
+        double dubNum = in.getDouble();
+        System.out.println("Your number is: " + dubNum);
+
+
+
+    }
+
+
+}
