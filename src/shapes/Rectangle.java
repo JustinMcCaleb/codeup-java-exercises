189cff03d9e955c62135dc052a273bbc34412cbf
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-16 16:03:22 -0500

    Interfaces and Abstract classes: completed exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,20 +1,51 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral implements Measurable{
 
-    protected double length;
-    protected double width;
+    //Code from inheritance and polymorphism exercise
+        //    protected double length;
+        //    protected double width;
+        //
+        //    public Rectangle(double l, double w) {
+        //        this.length = l;
+        //        this.width = w;
+        //    }
+        //
+        //    public double getArea() {
+        //        return length * width;
+        //    }
+        //
+        //    public double getPerimeter() {
+        //        return ((2 * length) + (2 * width));
+        //    }
 
-    public Rectangle(double l, double w) {
-        this.length = l;
-        this.width = w;
-    }
 
-    public double getArea() {
-        return length * width;
-    }
 
-    public double getPerimeter() {
-        return ((2 * length) + (2 * width));
-    }
+    //Interfaces and Abstract Classes exercise
+
+
+            public Rectangle(double length, double width) {
+                super(length, width);
+            }
+
+
+            @Override
+            void setLength(double length) {
+                this.length = length;
+            }
+
+            @Override
+            void setWidth(double width) {
+                this.width = width;
+            }
+
+            @Override
+            public double getPerimeter() {
+                return ((2 * length) + (2 * width));
+            }
+
+            @Override
+            public double getArea() {
+                return length * width;
+            }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,16 +1,49 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
 
-    public Square(double side) {
-        super(side, side);
-    }
+    //Code from inheritance and polymorphism exercise
+        //    public Square(double side) {
+        //        super(side, side);
+        //    }
+        //
+        //    public double getArea() {
+        //        return Math.pow(width, 2);
+        //    }
+        //
+        //    public double getPerimeter() {
+        //        return 4 * width;
+        //    }
 
-    public double getArea() {
-        return Math.pow(width, 2);
-    }
 
-    public double getPerimeter() {
-        return 4 * width;
-    }
+
+    //Interfaces and Abstract Classes exercise
+
+
+            public Square(double side) {
+                super(side, side);
+            }
+
+
+            @Override
+            void setLength(double length) {
+                this.length = length;
+                this.width = length;
+            }
+
+            @Override
+            void setWidth(double width) {
+                this.width = width;
+                this.length = width;
+            }
+
+            @Override
+            public double getPerimeter() {
+                return ((2 * length) + (2 * width));
+            }
+
+            @Override
+            public double getArea() {
+                return length * width;
+            }
 }
