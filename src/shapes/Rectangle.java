3d3d4def98c0c9df4c93e7e3970bb1db97b275ab
Justin McCaleb <justinleemccaleb@gmail.com>
2020-06-16 09:34:47 -0500

    Inheritance and Polymorphism: completed exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,20 @@
+package shapes;
+
+public class Rectangle {
+
+    protected int length;
+    protected int width;
+
+    public Rectangle(int l, int w) {
+        this.length = l;
+        this.width = w;
+    }
+
+    public int getArea() {
+        return length * width;
+    }
+
+    public int getPerimeter() {
+        return ((2 * length) + (2 * width));
+    }
+}
